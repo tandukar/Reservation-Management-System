6d98167541f52e36b143e3feb68be0b00fdd856b
tandukar <84461389+tandukar@users.noreply.github.com>
2023-06-11 11:18:25 +0545

    configuring for netlify

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        outDir: 'dist', // Specify the output directory for the production build
+        // Add any other build-specific configurations if needed
+    },
 })

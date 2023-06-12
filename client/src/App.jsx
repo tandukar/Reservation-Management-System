a0e9d98a2adf2d0f36bd721db7246dcbc74a6bbe
tandukar <84461389+tandukar@users.noreply.github.com>
2023-06-12 22:14:11 +0545

    fixing build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import Login from "./components/pages/auth/Login";
 import SendOtp from "./components/pages/auth/SendOtp";
 import VerifyOtp from "./components/pages/auth/VerifyOtp";
@@ -6,21 +12,33 @@ import ResetPwd from "./components/pages/auth/Resetpwd";
 import Dashboard from "./components/pages/dashboard/Dashboard";
 import "./components/pages/calendar/Reservations.css";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    // <Route path="/" element={<Navigate to="/login" />}>
+    <Route path="/">
+    <Route path="login" element={<Login />}/>
+      <Route path="send-otp" element={<SendOtp />} />
+      <Route path="verify-otp" element={<VerifyOtp />} />
+      <Route path="reset-password" element={<ResetPwd />} />
+      <Route path="dashboard" element={<Dashboard />} />
+    </Route>
+  )
+);
 
 function App() {
- 
   return (
     <>
-      <BrowserRouter>
+      <RouterProvider router={router} />
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/send-otp" element={<SendOtp/>} />
-          <Route path="/verify-otp" element={<VerifyOtp/>} />
-          <Route path="/reset-password" element={<ResetPwd/>} />
-          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/send-otp" element={<SendOtp />} />
+          <Route path="/verify-otp" element={<VerifyOtp />} />
+          <Route path="/reset-password" element={<ResetPwd />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </>
   );
 }

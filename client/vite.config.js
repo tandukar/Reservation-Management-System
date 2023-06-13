fecd7a8259f0defc59fd849400c6f07625c3c671
tandukar <84461389+tandukar@users.noreply.github.com>
2023-06-13 08:18:41 +0545

    fixing build

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    resolve: {
+        alias: {
+            'react': 'https://cdn.skypack.dev/react@17',
+            'react-dom': 'https://cdn.skypack.dev/react-dom@17'
+        }
+    },
+    plugins: [reactRefresh()],
     build: {
         outDir: 'dist',
         rollupOptions: {

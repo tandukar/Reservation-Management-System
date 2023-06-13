827f4a09ad837d69826936ae2a7bec69d1b666b1
tandukar <84461389+tandukar@users.noreply.github.com>
2023-06-13 09:08:32 +0545

    fixing build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+// import {
+//   Route,
+//   createBrowserRouter,
+//   createRoutesFromElements,
+//   RouterProvider,
+//   Navigate,
+// } from "react-router-dom";
 import Login from "./components/pages/auth/Login";
 import SendOtp from "./components/pages/auth/SendOtp";
 import VerifyOtp from "./components/pages/auth/VerifyOtp";
@@ -13,24 +13,24 @@ import ResetPwd from "./components/pages/auth/Resetpwd";
 import Dashboard from "./components/pages/dashboard/Dashboard";
 import "./components/pages/calendar/Reservations.css";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="">
-      <Route path="" element={<Login />} />
-      <Route path="send-otp" element={<SendOtp />} />
-      <Route path="verify-otp" element={<VerifyOtp />} />
-      <Route path="reset-password" element={<ResetPwd />} />
-      <Route path="dashboard" element={<Dashboard />} />
-    </Route>
-  )
-);
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="">
+//       <Route path="" element={<Login />} />
+//       <Route path="send-otp" element={<SendOtp />} />
+//       <Route path="verify-otp" element={<VerifyOtp />} />
+//       <Route path="reset-password" element={<ResetPwd />} />
+//       <Route path="dashboard" element={<Dashboard />} />
+//     </Route>
+//   )
+// );
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
 
-      {/* <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
@@ -39,7 +39,7 @@ function App() {
           <Route path="/reset-password" element={<ResetPwd />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-      </BrowserRouter> */}
+      </Router>
     </>
   );
 }

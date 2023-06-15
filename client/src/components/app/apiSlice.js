0d2086ca46c0b53613d5c709dd3a1b0476fbaabf
tandukar <84461389+tandukar@users.noreply.github.com>
2023-06-15 18:35:17 +0545

    integrating api

--- client/src/components/app/apiSlice.js
+++ client/src/components/app/apiSlice.js
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/',
+    baseUrl: 'https://sgh-reservation.onrender.com',
+    // baseUrl: 'http://localhost:5000/',
+
     prepareHeaders: (headers) => {
         const token = localStorage.getItem('token');
 

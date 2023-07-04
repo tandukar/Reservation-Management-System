4d62878c531f823ca767830da8f60e32dbef4cc3
tandukar <84461389+tandukar@users.noreply.github.com>
2023-07-04 19:04:14 +0545

    changing server api

--- client/src/components/app/apiSlice.js
+++ client/src/components/app/apiSlice.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-    // baseUrl: 'https://sgh-reservation.onrender.com',
-    baseUrl: 'http://localhost:5000/',
+    baseUrl: 'https://sgh-reservation.onrender.com',
+    // baseUrl: 'http://localhost:5000/',
 
     prepareHeaders: (headers) => {
         const token = localStorage.getItem('token');
